cc654d389edc07144ef84ecc5de5d6f77c5995ea
Adel del Valle <adel.delvalle@upr.edu>
2024-12-12 12:55:18 -0500

    cleaned main.py code

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from lsh import LSH
 import torch
 import numpy as np
-import math
 import torch.nn as nn
 import torch.nn.functional as F
 from simHash import SimHash
@@ -11,11 +10,11 @@ from sklearn.preprocessing import StandardScaler
 import time
 from fvcore.nn import FlopCountAnalysis
 import utils
-
 import matplotlib.pyplot as plt
+from hashedFC import HashedFC
+
 
 device = torch.device("cuda:0")
-from hashedFC import HashedFC
 
 
 class HashedNetwork(nn.Module):
@@ -48,18 +47,18 @@ class HashedNetwork(nn.Module):
 
 
         x = F.relu(self.fc1(x))
-        self.fc1.accumulate_metrics(x)  # Accumulate activations for the weighted average
+        self.fc1.accumulate_metrics(x)  # Accumulate HashedFC1 activations for the weighted average
         x = F.relu(self.fc2(x))
-        self.fc2.accumulate_metrics(x)
+        self.fc2.accumulate_metrics(x)   # Accumulate HashedFC2 activations for the weighted average
         x = F.relu(self.fc3(x))
-        self.fc3.accumulate_metrics(x)
+        self.fc3.accumulate_metrics(x)   # Accumulate HashedFC3 activations for the weighted average
         x = self.fc4(x)
-        self.fc4.accumulate_metrics(x)
-
+        self.fc4.accumulate_metrics(x)   # Accumulate HashedFC4 activations for the weighted average
         return x
 
 
 class VanillaNetwork(nn.Module):
+    """Standard Vanilla network"""
     def __init__(self, input_dim, hidden_dim, output_dim):
         super(VanillaNetwork, self).__init__()
         self.fc1 = nn.Linear(input_dim, hidden_dim)
@@ -75,23 +74,20 @@ class VanillaNetwork(nn.Module):
         return x
 
 
-
 # Train function
 def train_model(model, optimizer, criterion, X_train, y_train, epochs=29, prune_every=10):
     """
     Train the model and prune hashed layers every 'prune_every' epochs.
     """
     loss_history = []
-
     model.train()
     for epoch in range(epochs):
         # Dynamically prune and adjust hashed layers every 'prune_every' epochs
         rehash = isinstance(model, HashedNetwork) and (epoch + 1) % prune_every == 0
         model.rehash = rehash
-        
-
         optimizer.zero_grad()
         output = model(X_train)
+
         if rehash:
             # -------------------- Temporal solution while thinking of how to modify the optimizer/gradient parameters in place  ----------- #
             # --------------------  We clean the param groups because the optimizer will try to optimize the pruned params ----------- #
@@ -112,15 +108,13 @@ def train_model(model, optimizer, criterion, X_train, y_train, epochs=29, prune_
             new_params = model.fc4.params.parameters()
             optimizer.add_param_group({'params': new_params})
 
-
-        #monitor_gradients(model)
         loss = criterion(output, y_train)
         loss.backward()
         print(f"Epoch {epoch+1} Loss: {loss}")
         torch.nn.utils.clip_grad_norm_(model.parameters(), max_norm=1.0)
         loss_history.append(loss.item())
-
         optimizer.step()
+
     return loss_history
        
 
@@ -136,7 +130,6 @@ def measure_accuracy(model, X, y):
     return accuracy
 
 
-# Main comparison
 def compare_networks():
     """ Method to compare networks. It creates an instance of every network, and trains them
      with the same parameters, optimizers, struture, and data.  """
@@ -146,13 +139,7 @@ def compare_networks():
 
     # Generate data
     X, y = utils.generate_synthetic_data(n_samples=10000, n_features=input_dim, n_classes=output_dim)
-    #X, y = get_higgs_small_dataset()
-
-    
-    #X_train, y_train = X.to(device), y.to(device)
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)  # Split data 
-    
-
     X_train, X_test = torch.tensor(X_train, dtype=torch.float32).to(device), torch.tensor(X_test, dtype=torch.float32).to(device)
     y_train, y_test = torch.tensor(y_train, dtype=torch.long).to(device), torch.tensor(y_test, dtype=torch.long).to(device)
 
@@ -161,7 +148,6 @@ def compare_networks():
     hashed_optimizer = torch.optim.Adam(hashed_model.parameters(), lr=0.001)
     hashed_criterion = nn.CrossEntropyLoss()
     #wandb.init(project="hashed-vs-vanilla", name="HashedNetworkAnalysis")
-
     start_time = time.time()
 
     # --------------------------- Training of Hashed Neural Network --------------------------- #
@@ -172,7 +158,6 @@ def compare_networks():
     hashed_test_accuracy = measure_accuracy(hashed_model, X_test, y_test)
     #wandb.finish()
 
-
     # --------------------------- Training of Vanilla Neural Network --------------------------- #
 
     vanilla_model = VanillaNetwork(X.shape[1], hidden_dim, output_dim).to(device)
@@ -181,7 +166,6 @@ def compare_networks():
     #wandb.init(project="hashed-vs-vanilla", name="VanillaNetworkAnalysis")
 
     start_time = time.time()
-    
     vanilla_loss_history = train_model(vanilla_model, vanilla_optimizer, vanilla_criterion, X_train, y_train)
     vanilla_time = time.time() - start_time
     vanilla_accuracy = measure_accuracy(vanilla_model, X_train, y_train)
@@ -193,26 +177,21 @@ def compare_networks():
     print(f"Hashed Network Training Time: {hashed_time:.2f}s")
     print(f"Hashed Network Accuracy: {hashed_accuracy:.2f}%")
     print(f"Hashed Network - Test Accuracy: {hashed_test_accuracy:.2f}%")
-
     flops = FlopCountAnalysis(hashed_model, torch.randn(1, X.shape[1]).to(device))
     print(f"Hashed Network FLOPS: {flops.total()}")
     print_total_parameters(hashed_model, model_name="Hashed Network")
-
     flops = FlopCountAnalysis(vanilla_model, torch.randn(1, X.shape[1]).to(device))
-
     print(f"Vanilla Network Training Time: {vanilla_time:.2f}s")
     print(f"Vanilla Network Accuracy: {vanilla_accuracy:.2f}%")
     print(f"Vanilla Network - Test Accuracy: {vanilla_test_accuracy:.2f}%")
-
     print(f"Vanilla Network FLOPS: {flops.total()}")
     print_total_parameters(vanilla_model, model_name="Vanilla Network")
-
-
     # Plotting
     utils.plot_results(hashed_loss_history, vanilla_loss_history)
     utils.plot_layerwise_weight_distribution(hashed_model, "HashedNN")
     utils.plot_layerwise_weight_distribution(vanilla_model, "VanillaNN")
 
+    # -------------------------- Finished Printing Stats/Plots -------------------------------- #
 
 
 def print_total_parameters(model, model_name="Model"):
@@ -220,7 +199,6 @@ def print_total_parameters(model, model_name="Model"):
     trainable_params = sum(p.numel() for p in model.parameters() if p.requires_grad)
     print(f"{model_name} - Total Parameters: {total_params:,}")
     print(f"{model_name} - Trainable Parameters: {trainable_params:,}")
-
 
 
 if __name__ == "__main__":
